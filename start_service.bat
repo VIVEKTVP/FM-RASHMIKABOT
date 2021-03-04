dd654d41a7622ab609b609ea4043304486f8e8bd
VIVEKTVP <76048835+VIVEKTVP@users.noreply.github.com>
2021-03-04 18:42:43 +0530

    Update start_service.bat

--- start_service.bat
+++ start_service.bat
@@ -4,7 +4,7 @@
 :: BatchGotAdmin
 :-------------------------------------
 REM  --> Check for permissions
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+>nul 2>&1 "%SYSTEMROOT%\system32\cacl.exe" "%SYSTEMROOT%\system32\config\system"
 
 REM --> If error flag set, we do not have admin.
 if '%errorlevel%' NEQ '0' (
